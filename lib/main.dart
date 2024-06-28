abab63a35168ba66fb5fda6a25b2533b79a86edc
France! <franciscodomingosjose06798@gmail.com>
2024-06-28 15:55:48 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'dart:async';
 
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_health_app/firebase_options.dart';
 import 'package:my_health_app/splash_view.dart';
 
-main(){
-
+main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlataform
+  );
  runApp(MyWidget()); 
 }
 
@@ -14,7 +19,22 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Splash(),
+      title: 'Medtec',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            return Home()
+          }else{
+
+          }
+        }
+      ),
     );
   }
 }
