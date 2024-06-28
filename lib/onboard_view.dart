de7dc97b7a4e6f3e629dfa059f58a0d57140c204
France! <franciscodomingosjose06798@gmail.com>
2024-06-28 20:53:00 +0100

    add pageView builder on onboardView

--- lib/onboard_view.dart
+++ lib/onboard_view.dart
@@ -11,7 +11,54 @@ class _OnboardViewState extends State<OnboardView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: PageView.builder(
+              controller: pageController,
+              itemCout: listOfItems.length,
+              onPageChanged: (newIndex){
+                setState(
+                  (){
+                    currentIndex = newIndex;
+                  }
+                );
+              },
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: ((context, index){
+                return Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(15, 40, 15, 10),
+                      width: 500,
+                      height: 325,
+                      child: CustomWidgetAnimated(
+                        index: index,
+                        delay: 100,
+                        child: Image.asset(listOfItems[index].img),
+                      )
+                    ),
+                    const SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: CustomWidgetAnimated(
+                        index: index,
+                        delay: 300,
+                        child: Text(
+                          listOfItems[index].title,
+                          textAlign: TextAlign.center,
+                          style: 
+                        )
+                      ),
+                      )
+                  ]
+                );
+              })
+            )
+          )
+        ]
+      ),
     );
   }
 }
