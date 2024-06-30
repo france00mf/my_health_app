516b2fac516183c4ac45788f94905d97a1b04150
France! <franciscodomingosjose06798@gmail.com>
2024-06-30 11:55:40 +0100

    add email field

--- lib/presenter/view/auth/register/register_view.dart
+++ lib/presenter/view/auth/register/register_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_health_app/core/app_constants/theme/theme.dart';
+import 'package:my_health_app/core/presenters/components/primary_text_form_field.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -22,7 +23,6 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     String name;
-
 
     return Scaffold(
       backgroundColor: AppColor.kWhite,
@@ -45,6 +45,100 @@ class _RegisterViewState extends State<RegisterView> {
                     fontWeight: FontWeight.w600,
                     fontSize: 24)
             ),
+            const SizedBox(height: 8),
+            Text('Feliz por te ver, regista-se e use a app',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.plusJakartaSans(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: AppColor.kWhite)
+                    .copyWith(
+                        color: AppColor.kGrayscale40,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Primeiro Nome',
+                    style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: AppColor.kWhite)
+                            .copyWith(
+                                color: AppColor.kGrayscaleDark100,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14),
+                    ),
+                    const SizedBox(height: 8,),
+                    PrimaryTextFormField(
+                      borderRadius: BorderRadius.circular(24),
+                      hintText: 'Primeiro',
+                      controller: firstName,
+                      width: 155,
+                      height: 52
+                    )
+                  ]
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Ultimo Nome',
+                           style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: AppColor.kWhite)
+                            .copyWith(
+                                color: AppColor.kGrayscaleDark100,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14),
+                    ),
+
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  PrimaryTextFormField(
+                    borderRadius: BorderRadius.circular(24),
+                    hintText: 'Ultimo',
+                    controller: listName,
+                    width: 155,
+                    height: 52
+                  )
+                  ]
+                )
+              ]
+            ),
+
+            SizedBox(
+              height: 15,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                  Text("Email",
+                          style: GoogleFonts.plusJakartaSans(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: AppColor.kWhite)
+                        .copyWith(
+                            color: AppColor.kGrayscaleDark100,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14),
+                  ),
+                  const SizedBox(height: 7,),
+                   PrimaryTextFormField(
+                      borderRadius: BorderRadius.circular(24),
+                      hintText: 'francisco.jose@gmail.com',
+                      controller: emailController,
+                      width: 327,
+                      height: 52),
+              ],
+            )
+
           ],
         ),
       ),
