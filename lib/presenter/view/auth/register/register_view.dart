a18424e6fccb542b6a92112b86df62902b22e662
France! <franciscodomingosjose06798@gmail.com>
2024-06-30 09:15:03 +0100

    add main text on register page

--- lib/presenter/view/auth/register/register_view.dart
+++ lib/presenter/view/auth/register/register_view.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_health_app/core/presenters/components/theme/theme.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -15,10 +17,37 @@ class _RegisterViewState extends State<RegisterView> {
   TextEditingController passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  
+
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String name;
+
+
+    return Scaffold(
+      backgroundColor: AppColor.kWhite,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Text('Criar Conta',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.plusJakartaSans(
+              fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ).copyWith(
+                    color: AppColor.kGrayscaleDark100,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 24)
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
